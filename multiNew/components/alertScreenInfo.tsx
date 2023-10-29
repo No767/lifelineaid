90004e2c918944d0bc80839e799100f9a9b2e3c4
sanityl0st <snarns5@gmail.com>
2023-10-29 05:45:03 -0700

    updated alerts screen

--- multiNew/components/alertScreenInfo.tsx
+++ multiNew/components/alertScreenInfo.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet , ScrollView} from 'react-native';
 import Colors from '../constants/Colors';
 import { ExternalLink } from './ExternalLink';
 import { MonoText } from './StyledText';
@@ -10,16 +10,14 @@ import { Text, View } from './Themed';
 
 export default function EditScreenInfo({ path }: { path: string }) {
   return (
-    <View>
-      <View style={styles.card}>
-        
-              
+    <ScrollView>
+      <View style={styles.getStartedContainer}>
+        <View style={styles.card}>
               <View style={styles.header}> 
                   <Text style={styles.title}> 
                       Medical Center at Shansha
                   </Text> 
               </View> 
-
               <View style={styles.content}> 
                   <Text style={styles.text}> 
                     Can supply first aid care, shelter, medicine, and relay to rescue organizations
@@ -27,23 +25,23 @@ export default function EditScreenInfo({ path }: { path: string }) {
               </View> 
               
       <View/>
+                <View style={styles.divider} />
       
-      <View style={styles.card}>
+        <View style={styles.card}>
               <View style={styles.header}> 
-                  <Text style={styles.title}> 
-                      Medical Center at Shansha
+                  <Text style={styles.title2}> 
+                      Earthquake Warning
                   </Text> 
               </View> 
-                
               <View style={styles.content}> 
                   <Text style={styles.text}> 
-                    Can supply first aid care, shelter, medicine, and relay to rescue organizations
+                    Seek shelter! Earthquake Incoming!
                   </Text> 
               </View> 
           </View> 
         </View>
-       
-    </View>
+      </View>
+    </ScrollView>
   );
 }
 
@@ -51,16 +49,24 @@ const styles = StyleSheet.create({
   getStartedContainer: {
     alignItems: 'center',
     marginHorizontal: 50,
+    flex: 1,
   },
   card: {
     backgroundColor: 'white',
-    borderRadius: 15,
+    borderRadius: 2,
     padding: 16,
     shadowColor: 'black',
     shadowOffset: {
-      width: 0,
-      height: 4,
+      width: 2,
+      height: 2,
     }
+    
+  },
+  divider: {
+    borderBottomColor: 'gray',
+    borderBottomWidth: 4,
+    marginTop: 50,
+    marginBottom: 12,
   },
   header: { 
     marginBottom: 16, 
@@ -70,6 +76,11 @@ const styles = StyleSheet.create({
     fontSize: 25  , 
     fontWeight: 'bold', 
     color: 'green', 
+  }, 
+  title2: { 
+    fontSize: 25  , 
+    fontWeight: 'bold', 
+    color: 'red', 
   }, 
   subtitle: { 
     fontSize: 24, 
@@ -88,8 +99,8 @@ const styles = StyleSheet.create({
     marginVertical: 7,
   },
   codeHighlightContainer: {
-    borderRadius: 3,
-    paddingHorizontal: 4,
+    borderRadius: 1,
+    paddingHorizontal: 2,
   },
   getStartedText: {
     fontSize: 17,
